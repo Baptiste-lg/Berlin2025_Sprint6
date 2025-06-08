b53e1bc13af293b5011102d499d298c60170f20c
Baptiste Le Glaunec <baptiste.le-glaunec@epitech.eu>
2025-06-08 18:02:33 +0200

    [FEAT]: Add GameMode for second map

---
+++ Source/Berlin2025_Sprint6/MyFPSGameMode.cpp
@@ -0,0 +1,27 @@
+﻿#include "MyFPSGameMode.h"
+#include "Kismet/GameplayStatics.h"
+#include "FPSPlayer_BaseCpp.h" // A besoin de connaître le joueur
+#include "Enemy_AI.h"          // et l'ennemi
+
+void AMyFPSGameMode::ResetPlayerAndEnemies(AController* PlayerController)
+{
+	UE_LOG(LogTemp, Warning, TEXT("--- GAME MODE: Resetting level state. ---"));
+
+	// 1. Réinitialiser le joueur
+	if (AFPSPlayer_BaseCpp* Player = Cast<AFPSPlayer_BaseCpp>(PlayerController->GetPawn()))
+	{
+		Player->ResetToStartLocation();
+	}
+
+	// 2. Réinitialiser tous les ennemis
+	TArray<AActor*> FoundEnemies;
+	UGameplayStatics::GetAllActorsOfClass(GetWorld(), AEnemy_AI::StaticClass(), FoundEnemies);
+
+	for (AActor* Actor : FoundEnemies)
+	{
+		if (AEnemy_AI* Enemy = Cast<AEnemy_AI>(Actor))
+		{
+			Enemy->ResetToStartLocation();
+		}
+	}
+}
---
+++ Source/Berlin2025_Sprint6/MyFPSGameMode.h
@@ -0,0 +1,15 @@
+﻿#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "MyFPSGameMode.generated.h"
+
+UCLASS()
+class BERLIN2025_SPRINT6_API AMyFPSGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	// Fonction publique qui peut être appelée de n'importe où pour tout réinitialiser
+	void ResetPlayerAndEnemies(AController* PlayerController);
+};
